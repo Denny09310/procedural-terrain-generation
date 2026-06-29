7c5b6e7639d705af4732ac6f49bb89d59a30ecbe
Denny09310 <k.denny2000@gmail.com>
2026-06-29 14:06:17 +0200

    chore: removed viewer

---
+++ sample/Program.cs
@@ -0,0 +1,27 @@
+﻿using Core.Models;
+using Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = TerrainGeneratorBuilder.Create();
+
+builder.Services.AddSingleton(_ => new TerrainConfiguration
+{
+    ChunkSize = 128
+});
+
+builder.AddTransformer((TerrainGrid grid, TerrainContext ctx, ElevationTransformer elevation)
+    => elevation.Apply(grid, ctx));
+
+builder.AddTransformer((TerrainGrid grid, TerrainContext ctx, MoistureTransformer moisture)
+    => moisture.Apply(grid, ctx));
+
+builder.AddTransformer((TerrainGrid grid, TerrainContext ctx, TemperatureTransformer temperature)
+    => temperature.Apply(grid, ctx));
+
+builder.AddTransformer((TerrainGrid grid, ClassifierTransformer classifier)
+    => classifier.Apply(grid));
+
+var generator = builder.Build();
+
+var chunk = await generator.GenerateAsync(0, 0);
+System.Console.WriteLine();
